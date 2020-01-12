1e894ed58a780b31056c2fc254d3b87306a3d8c3
Michal <michal.thor5@gmail.com>
2020-01-12 18:38:55 +0100

    Add df_corr

--- master_function_thor.R
+++ master_function_thor.R
@@ -2,36 +2,11 @@ library(plotly)
 library(xts)
 library(dplyr)
 library(tidyr)
-library(zoo)
 source("./functions/functions_thor.R")
 
-# Getting Reddit csv to work
-# prepareReddit <- function(file_name = "../data/daily_sentiment.csv") {
-#   df_redd <- read.csv(file_name)
-#   df_redd$date <- as.Date(df_redd$date)
-#   df_redd$date <- as.POSIXct(strptime(df_redd$date, "%Y-%m-%d"))
-#   df_redd$ave_sentiment <- as.numeric(as.character(df_redd$ave_sentiment))
-#   df_redd$X <- NULL
-#   qxts <- xts(df_redd[,2], order.by=df_redd[,1])
-#   write.zoo(qxts,file="reddit_sentiment.csv",index.name="date",row.names=FALSE,col.names=TRUE,sep=",")
-# }
-
-# accumulateBy <- function(dat, var) {
-#   var <- lazyeval::f_eval(var, dat)
-#   lvls <- plotly:::getLevels(var)
-#   dats <- lapply(seq_along(lvls), function(x) {
-#     cbind(dat[var %in% lvls[seq(1, x)], ], frame = lvls[[x]])
-#   })
-#   dplyr::bind_rows(dats)
-# }
-
-# csvToXTS <- function(csv_file){
-#   
-#   zoo_file <- read.zoo(csv_file, sep=",", header = TRUE, tz = "")
-#   xts_file <- as.xts(zoo_file)
-#   
-#   return(xts_file)
-# }
+##############################################################
+# I Multiple plots with Sentiment
+##############################################################
 
 xts1 <- csvToXTS("./data/merged_indices.csv")
 index(xts1) <- round(index(xts1), "days")
@@ -46,45 +21,145 @@ colnames(xts_reddit) <- c("Sentiment")
 xts_merged_small <- merge.xts(xts1, xts_reddit) %>% 
   na.omit()
 
-# xtsToDF <- function(xts_file) {
-#   
-#   df <- data.frame(xts_file)
-#   df <- cbind(date_time = rownames(df), df)
-#   df$ID <- seq.int(nrow(df))
-#   
-#   names(df) <- gsub(".Open", "", names(df), fixed = TRUE)
-#   
-#   return (df)
-# }
+xts_corr <- merge.xts(xts1, xts_reddit) %>% 
+  na.locf(.) %>% 
+  na.omit()
 
 dfm <- xtsToDF(xts1)
 dfm_small <- xtsToDF(xts_merged_small)
 
-
-# plotMultiple <- function(df_file, index_1, index_2, index_3) {
-#   p <- df_file %>%
-#     select(., date_time, index_1, index_2, index_3) %>% 
-#     pivot_longer(-date_time, names_to = "variable", values_to = "value") %>% 
-#     transform(id = as.integer(factor(variable))) %>%
-#     plot_ly(.,
-#       x = ~date_time, 
-#       y = ~value,
-#       color = ~variable,
-#       colors = "Dark2",
-#       yaxis = ~paste0("y", id)
-#     ) %>%
-#     add_lines() %>%
-#     subplot(nrows = 3, shareX = TRUE) %>% 
-#     layout(
-#       title = "Prices of 3 chosen indices since June 2019",
-#       xaxis = list(
-#         type = "Date",
-#         tickformat = "%Y-%m"
-#       ))
-#  
-#   return (p)
-# }
+df_corr <- xtsToDF(xts_corr)
     
 plotMultiple(dfm, "GE", "ZG", "LMT")
 
 plotMultiple(dfm_small, "GE", "HAL", "Sentiment")
+
+##############################################################
+# II part - self-drawing plots with plotly
+##############################################################
+
+z <- read.zoo("./data/merged_indices.csv", sep=",", header = TRUE, tz = "")
+data_x <- as.xts(z)
+
+df_x <- data.frame(data_x)
+df_x <- cbind(date_time = rownames(df_x), df_x)
+df_x$ID <- seq.int(nrow(df_x))
+
+
+
+df_x_acc <- df_x %>%
+  accumulateBy(~ID)
+
+
+# write.zoo(data_x,file="merged_indices.csv",index.name="date",row.names=FALSE,col.names=TRUE,sep=",")
+
+r <- read.zoo("./data/reddit_xts.csv", sep=",", header = TRUE, tz = "")
+data_r <- as.xts(r)
+df_r <- data.frame(data_r)
+df_r <- cbind(date_time = rownames(df_r), df_r)
+df_r$ID <- seq.int(nrow(df_r))
+
+df_r_acc <- df_r %>%
+  accumulateBy(~ID)
+
+triple_underlying_plot <- df_x_acc %>%
+  plot_ly(
+    x = ~ID, 
+    y = ~XAR.Open, 
+    frame = ~frame,
+    type = 'scatter', 
+    mode = 'lines',
+    name="OpenXAR",
+    line = list(color = 'rgb(114, 186, 59)'),
+    text = ~paste(date_time, "<br>Close: $", XAR.Open), 
+    hoverinfo = 'text'
+  )  %>% 
+  add_trace(
+    y = ~ZG.Open, 
+    name = 'Open',
+    line = list(color = 'rgb(153, 186, 81)'),
+    mode = 'lines',
+    text = ~paste(date_time, "<br>Open: $", ZG.Open) 
+  ) %>%
+  add_trace(
+    y = ~GE.Open, 
+    name = 'Open', 
+    mode = 'lines',
+    line = list(color = 'rgb(100, 82, 159)'),
+    text = ~paste(date_time, "<br>Close: $", GE.Open) 
+  ) %>%
+  add_trace(
+    y = ~EUR.USD, 
+    name = 'EUR/USD', 
+    mode = 'lines',
+    line = list(color = 'rgb(163, 26, 59)'),
+    text = ~paste(date_time, "<br>Close: $", EUR.USD) 
+  ) %>%
+  layout(
+    title = "Values of chosen underlyings since July 2019",
+    yaxis = list(
+      title = "Close", 
+      range = c(30,60), 
+      zeroline = F,
+      tickprefix = "$"
+    ),
+    xaxis = list(
+      title = "Day", 
+      range = c(0,130), 
+      zeroline = F, 
+      showgrid = F
+    )
+  ) %>% 
+  animation_opts(
+    frame = 40, 
+    transition = 7, 
+    redraw = FALSE
+  ) %>%
+  animation_slider(
+    currentvalue = list(
+      prefix = "Day "
+    )
+  )
+
+sentiment_plot <- df_r_acc %>%
+  plot_ly(
+    x = ~ID, 
+    y = ~data_r, 
+    frame = ~frame,
+    type = 'scatter', 
+    mode = 'lines',
+    name= "Sentiment",
+    line = list(color = 'rgb(114, 186, 59)'),
+    text = ~paste(date_time, "<br>Value:", data_r), 
+    hoverinfo = 'text'
+  ) %>%
+  layout(
+    title = "Sentiment value since May 2019",
+    yaxis = list(
+      title = "Sentiment value", 
+      range = c(-0.8,0.8), 
+      zeroline = F
+    ),
+    xaxis = list(
+      title = "Day",
+      range = c(0,72), 
+      zeroline = F, 
+      showgrid = F
+    )
+  ) %>% 
+  animation_opts(
+    frame = 40, 
+    transition = 7, 
+    redraw = FALSE
+  ) %>%
+  animation_slider(
+    currentvalue = list(
+      prefix = "Day "
+    )
+  )
+
+triple_underlying_plot
+
+sentiment_plot
+
+
